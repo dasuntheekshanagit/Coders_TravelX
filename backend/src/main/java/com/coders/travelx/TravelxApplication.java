d4419da7bc63af1b5be9d83294e1cb60c22e6fe2
G5andeepD <e19063@eng.pdn.ac.lk>
2023-08-17 15:52:22 +0530

    backend files

---
+++ backend/src/main/java/com/coders/travelx/TravelxApplication.java
@@ -0,0 +1,13 @@
+package com.coders.travelx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelxApplication.class, args);
+	}
+
+}
